f58f9f006f0a2e59360fa3da3a8853a7afb95957
tranduythuat <thuattran1810@gmail.com>
2020-08-14 11:00:51 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function (){
     return view('welcome');
 });
+
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
